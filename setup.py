1e771f4af93a7ed41e76f8ef499ced6d34a0a278
nguyen599 <pnvmanh2123@gmail.com>
2025-10-11 13:20:21 +0700

    add NVSHMEM_ENABLE
    Signed-off-by: nguyen599 <pnvmanh2123@gmail.com>

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ if __name__ == '__main__':
     else:
         disable_nvshmem = False
 
+    if not os.getenv('NVSHMEM_ENABLE', None):
+        disable_nvshmem = True
     if not disable_nvshmem:
         assert os.path.exists(nvshmem_dir), f'The specified NVSHMEM directory does not exist: {nvshmem_dir}'
 
